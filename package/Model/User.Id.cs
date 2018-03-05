06ffbdbc41a4f36acb59e03dfda4ec016f8318dd
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-03-05 18:09:42 +0300

    Adding UserId ctor with id param and hiding default ctor

--- package/Model/User.Id.cs
+++ package/Model/User.Id.cs
@@ -7,5 +7,12 @@ namespace MidnightLizard.Commons.Domain.Model
 {
     public class UserId : DomainEntityId<string>
     {
+        protected UserId() : base()
+        {
+        }
+
+        public UserId(string id) : base(id)
+        {
+        }
     }
 }
