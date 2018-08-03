7f579360d77c0786651c017e225bb87b4ed248d3
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-08-03 16:58:32 +0300

    adding IMessagingQueue.CheckStatus():bool method

--- package/Interfaces/IMessagingQueue.cs
+++ package/Interfaces/IMessagingQueue.cs
@@ -5,6 +5,7 @@ namespace MidnightLizard.Commons.Domain.Interfaces
 {
     public interface IMessagingQueue
     {
+        bool CheckStatus();
         Task BeginProcessing(CancellationToken token);
         Task ResumeProcessing(CancellationToken token);
         Task PauseProcessing();
